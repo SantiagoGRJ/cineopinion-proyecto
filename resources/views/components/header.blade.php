b40a279b4ab059bd26c58debbd2e2eebb63ffc5d
SANTIAGO GRJ <santiago-2003-rojas@hotmail.com>
2024-02-23 23:42:09 -0600

    Acomodo de Opciones de Login

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -83,7 +83,55 @@
 
     <li class="navbar-link">
 
-                    <x-dropdown align="right" width="48">
+        <div class="navbar-link  sm:flex sm:items-center sm:ms-6">
+            <x-dropdown align="right" width="48">
+                <x-slot name="trigger">
+                    <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                        @if (Auth::user())
+                        {{ Auth::user()->name }}
+                        @else
+                        <a href="{{ route('login') }}" class="">Log in</a>
+                        @endif
+                        <div class="ms-1">
+                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                    </button>
+                </x-slot>
+
+               @if (Auth::user())
+               <x-slot name="content">
+                <x-dropdown-link :href="route('profile.edit')">
+                    {{ __('Profile') }}
+                </x-dropdown-link>
+
+                <!-- Authentication -->
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <x-dropdown-link :href="route('logout')"
+                        onclick="event.preventDefault();
+                                                        this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                    </x-dropdown-link>
+                </form>
+            </x-slot>
+               @else
+               <x-slot name="content">
+                <x-dropdown-link :href="route('register')">
+                    @if (Route::has('register'))
+                        {{ __('Register') }}
+                    @endif
+                </x-dropdown-link>
+              </x-slot>
+
+               @endif
+            </x-dropdown>
+        </div>
+
+
+{{-- <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button
                                 class="  inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md  ease-in-out duration-150">
@@ -104,10 +152,11 @@
                 clip-rule="evenodd" />
         </svg>
             </div>
+        </button>
+    </x-slot>
         @endif
 
-           </button>
-         </x-slot>
+
 
            @if (Auth::user())
         <x-slot name="content">
@@ -137,6 +186,7 @@
                 @endif
 
               </x-dropdown>
+--}}
     </li>
 
     </ul>
